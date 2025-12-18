91e30ece32cda1004ac853b1f3cee1e990ed6ec6
Adhin-Raj <adhin.raj@vonnue.com>
2025-12-18 17:52:40 +0530

    fix: fixed font family of forgot password text

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -37,10 +37,10 @@ export default function LoginScreen() {
           showPassword={showPassword}
         />
         <View style={styles.forgotPassword}>
-          <Text style={styles.linkText}>Forgot your password? </Text>
+          <Text style={[styles.linkText,{fontFamily:'medium-sans'}]}>Forgot your password? </Text>
           <Link
             href={"/"}
-            style={[styles.linkText, { textDecorationLine: "underline" }]}
+            style={[styles.linkText, { textDecorationLine: "underline" ,fontFamily:'sans'}]}
           >
             Reset your password
           </Link>
